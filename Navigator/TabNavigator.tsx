5f0cc4eb69b9f7ca92882e3586940262a614e897
looju <omofade2020@gmail.com>
2023-03-23 23:31:19 +0000

    Tab

--- Navigator/TabNavigator.tsx
+++ Navigator/TabNavigator.tsx
@@ -1,13 +1,16 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { CustomerScreens } from "../Screens/CustomerScreens";
+import { OrderScreens } from "./../Screens/OrderScreen";
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigator = () => {
   return (
     <Tab.Navigator>
-      
+      <Tab.Screen name="customers" component={CustomerScreens} />
+      <Tab.Screen name="orders" component={OrderScreens} />
     </Tab.Navigator>
   );
 };
--- Screens/CustomerScreens.tsx
+++ Screens/CustomerScreens.tsx
@@ -2,14 +2,12 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { useTailwind } from 'tailwind-rn/dist'
 
-const CustomerScreens = () => {
+export const CustomerScreens = () => {
 
-    
+
   return (
     <View>
       <Text>CustomerScreens</Text>
     </View>
   )
 }
-
-export default CustomerScreens
