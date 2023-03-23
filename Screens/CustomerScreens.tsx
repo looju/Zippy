ee165fe64a4d31e51cb3494fe18298ab34e22c84
looju <omofade2020@gmail.com>
2023-03-23 22:33:48 +0000

    Tailwind

---
+++ Screens/CustomerScreens.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CustomerScreens = () => {
+  return (
+    <View>
+      <Text>CustomerScreens</Text>
+    </View>
+  )
+}
+
+export default CustomerScreens
