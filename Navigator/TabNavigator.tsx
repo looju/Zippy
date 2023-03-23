cde93d490b404cdefdbe9c3de59062ce6b7094b6
looju <omofade2020@gmail.com>
2023-03-23 23:56:35 +0000

    customize tab

--- Navigator/TabNavigator.tsx
+++ Navigator/TabNavigator.tsx
@@ -15,7 +15,6 @@ const Tab = createBottomTabNavigator<TabStackParamList>();
 export const TabNavigator = () => {
   const navigation = useNavigation();
 
-  
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -23,7 +22,26 @@ export const TabNavigator = () => {
   }, []);
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Customers") {
+            iconName = "ios-information-circle-outline";
+            size = focused ? 20 : 15;
+          } else if (route.name === "Orders") {
+            iconName = "ios-list-outline";
+            size = focused ? 20 : 15;
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen name="Customers" component={CustomerScreens} />
       <Tab.Screen name="Orders" component={OrderScreens} />
     </Tab.Navigator>
