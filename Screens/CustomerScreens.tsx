cee00ec65c5e888f21b13c01d86878ab482a8dc4
looju <omofade2020@gmail.com>
2023-03-24 15:17:40 +0000

    Types definiton for nav

--- Screens/CustomerScreens.tsx
+++ Screens/CustomerScreens.tsx
@@ -1,28 +1,27 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
-import { useTailwind } from 'tailwind-rn/dist'
-import { CompositeNavigationProp, useNavigation } from '@react-navigation/native'
-import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
-import {NativeStackNavigationProp} from '@react-navigation/native-stack'
-import { TabStackParamList } from '../Navigator/TabNavigator'
-import { RootStackParamList } from '../Navigator/RootNavigator'
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
+import { useTailwind } from "tailwind-rn/dist";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { TabStackParamList } from "../Navigator/TabNavigator";
+import { RootStackParamList } from "../Navigator/RootNavigator";
 
-
-
-
-export type CustomerScreensNavigationProps=CompositeNavigationProp<
-BottomTabNavigationProp< TabStackParamList,'Customers' >,
-NativeStackNavigationProp<RootStackParamList>
-
->
+export type CustomerScreensNavigationProps = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Customers">,
+  NativeStackNavigationProp<RootStackParamList>
+>;
 
 export const CustomerScreens = () => {
-const tw=useTailwind()
-const navigation=useNavigation()
+  const tw = useTailwind();
+  const navigation = useNavigation();
 
   return (
     <ScrollView>
       <Text>CustomerScreens</Text>
     </ScrollView>
-  )
-}
+  );
+};
