45ad246dc8c8574173dc82bc72181bffab13b8ca
looju <omofade2020@gmail.com>
2023-03-27 15:01:05 +0100

    Res

--- typings.d.ts
+++ typings.d.ts
@@ -1,22 +1,31 @@
-type Customer={
-    email:String;
-    name:String
-}
+type Customer = {
+  email: string;
+  name: string;
+};
 
-type CustomerList={
-    name:ID;
-    value:Customer
-}
+type CustomerList = {
+  name: ID;
+  value: Customer;
+};
 
-type trackingItems={
-    customer_id: String
-    items: [Items]
-    customer:Customer
-}
+type trackingItems = {
+  customer_id: string;
+  items: [Items];
+  customer: Customer;
+};
 
-type Items={
-    item_id: ID
-    name: String
-    price: Number
-    quantity: Number
-}
+type Items = {
+  item_id: ID;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+type OrderResponse = {
+  value: Order;
+};
+
+type CustomerResponse = {
+  name: ID;
+  value: Customer;
+};
