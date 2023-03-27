15d66d3cd996006fa347824c1db139576c837efc
looju <omofade2020@gmail.com>
2023-03-27 13:34:25 +0100

    getOrders client

--- Graphql/queries.ts
+++ Graphql/queries.ts
@@ -1,13 +1,44 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery } from "@apollo/client";
 
-
-export const GetCustomers=gql `query GetCustomer{
-getCustomer{
-    value{
+export const GetCustomers = gql`
+  query GetCustomer {
+    getCustomer {
+      value {
         email
         name
+      }
+      name
     }
-    name
-}
-}`
+  }
+`;
 
+export const GetOrders = gql`
+  query GetOrders {
+    getOrders {
+      name
+      value {
+        Address
+        Lat
+        City
+        Lng
+        carrier
+        createdAt
+        shippingCost
+        trackingId
+        trackingItems {
+          customer_id
+          customers {
+            email
+            name
+          }
+          items {
+            item_id
+            name
+            price
+            quantity
+          }
+        }
+      }
+    }
+  }
+`;
