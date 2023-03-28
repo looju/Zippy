07f8d0f24d32d2b15185b730803d466770223bd2
looju <omofade2020@gmail.com>
2023-03-29 00:45:55 +0100

    order type

--- Hooks/useCustomerOrders.tsx
+++ Hooks/useCustomerOrders.tsx
@@ -1,30 +1,17 @@
-import { View, Text } from "react-native";
-import React, { useState, useEffect } from "react";
-import { useQuery } from "@apollo/client";
-import { GetOrders } from "../Graphql/queries";
+import { View, Text } from 'react-native'
+import React,{useState} from 'react'
+import { useQuery } from '@apollo/client'
+import { GetOrders } from '../Graphql/queries'
 
-export const useCustomerOrders = () => {
-  const { loading, error, data } = useQuery(GetOrders);
-  const [orders, setOrders] = useState<Order[]>([]);
-
-  useEffect(() => {
-    if (!data) return;
-
-    const orders: Order = data.getOrders.map(({ value }: OrderResponse) => ({
-      carrier: value.carrier,
-      lat: value.Lat,
-      lng: value.Lng,
-      address: value.Address,
-      city: value.City,
-      createdAt: value.createdAt,
-      shippingCost: value.shippingCost,
-      trackingId: value.trackingId,
-    }));
-  }, [data]);
+const useCustomerOrders = () => {
+const {loading,error,data}=useQuery(GetOrders)
+const [orders,setOrders]=useState<Order[]>([])
 
   return (
     <View>
       <Text>useCustomerOrders</Text>
     </View>
-  );
-};
+  )
+}
+
+export default useCustomerOrders
---
+++ Hooks/useOrders.tsx
@@ -0,0 +1,28 @@
+import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { GetOrders } from "../Graphql/queries";
+
+export const useOrders = () => {
+  const { loading, error, data } = useQuery(GetOrders);
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+    if (!data) return;
+
+    const orders: Order[] = data.getOrders.map(({ value }: OrderResponse) => ({
+      carrier: value.carrier,
+      lat: value.Lat,
+      lng: value.Lng,
+      address: value.Address,
+      city: value.City,
+      createdAt: value.createdAt,
+      shippingCost: value.shippingCost,
+      trackingId: value.trackingId,
+    }));
+
+    setOrders(orders);
+  }, [data]);
+
+  return { loading, error, orders };
+};
