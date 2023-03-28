2d4d705b11876d56e5a3c23beee3a2e26258cb06
looju <omofade2020@gmail.com>
2023-03-29 00:10:21 +0100

    data value

--- Hooks/useCustomerOrders.tsx
+++ Hooks/useCustomerOrders.tsx
@@ -1,11 +1,19 @@
 import { View, Text } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { GetOrders } from "../Graphql/queries";
 
 export const useCustomerOrders = () => {
   const { loading, error, data } = useQuery(GetOrders);
-  const [orders,setOrders]=useState<Order[]>([])
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+    if (!data) return;
+
+    const orders:Order=data.getOrders.map(({value}:OrderResponse)=>({
+      carrier:value.carrier
+    }))
+  }, [data]);
 
   return (
     <View>
