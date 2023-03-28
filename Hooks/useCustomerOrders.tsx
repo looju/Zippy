ee3ec1cfae51759e96986ca147b070fdc56d55b7
looju <omofade2020@gmail.com>
2023-03-29 00:56:01 +0100

    Logic

--- Hooks/useCustomerOrders.tsx
+++ Hooks/useCustomerOrders.tsx
@@ -3,24 +3,30 @@ import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { GetOrders } from "../Graphql/queries";
 
-const useCustomerOrders = () => {
+const useCustomerOrders = (userId: string) => {
   const { loading, error, data } = useQuery(GetOrders);
   const [orders, setOrders] = useState<Order[]>([]);
 
   useEffect(() => {
     if (!data) return;
 
-   const orders:Order[]=data.getOrders.map(({value}:OrderResponse)=>({
-    carrier: value.carrier,
-    lat: value.Lat,
-    lng: value.Lng,
-    address: value.Address,
-    city: value.City,
-    createdAt: value.createdAt,
-    shippingCost: value.shippingCost,
-    trackingId: value.trackingId,
-    trackingItems:value.trackingItems
-   }))
+    const orders: Order[] = data.getOrders.map(({ value }: OrderResponse) => ({
+      carrier: value.carrier,
+      lat: value.Lat,
+      lng: value.Lng,
+      address: value.Address,
+      city: value.City,
+      createdAt: value.createdAt,
+      shippingCost: value.shippingCost,
+      trackingId: value.trackingId,
+      trackingItems: value.trackingItems,
+    }));
+
+    const customerOrder = orders.filter(
+      (value) => value.trackingItems.customer_id == userId
+    );
+
+    setOrders(customerOrder);
   }, [data]);
 
   return (
