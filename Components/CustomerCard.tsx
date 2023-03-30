3ac778c92dd00b1b27ff77e1ef326b189718d82c
looju <omofade2020@gmail.com>
2023-03-30 11:41:36 +0100

    CustomerOrders hook imp

--- Components/CustomerCard.tsx
+++ Components/CustomerCard.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useCustomerOrdersById } from './../Hooks/useCustomerOrdersById';
 
+type Props={
+    name:string
+    email:string
+    userId:string
+}
 
-export const CustomerCard=()=>{
-    
+export const CustomerCard=({name:email,userId}:Props)=>{
+  const  { loading, error, orders }=useCustomerOrdersById(userId)
+  
 }
