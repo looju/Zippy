c6ca97c0ac3ec3eb8cd5f1359e10f47200869c83
looju <omofade2020@gmail.com>
2023-03-31 11:15:54 +0100

    ParamList

--- Screens/ModalScreen.tsx
+++ Screens/ModalScreen.tsx
@@ -1,11 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import { useTailwind } from "tailwind-rn/dist";
+import { CompositeNavigationProp } from "@react-navigation/native";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../Navigator/RootNavigator";
+import { TabStackParamList } from "../Navigator/TabNavigator";
 
-export const ModalScreen = () => {
+type ModalScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList>,
+  NativeStackNavigationProp<RootStackParamList, "ModalScreen">
+>;
+
+export const ModalScreen = ({ route }) => {
+  const { name, userId } = route.params;
+  const tw = useTailwind();
+
   return (
-    <View>
-      <Text>ModalScreen</Text>
-    </View>
-  )
-}
-
+    <TouchableOpacity style={tw("absolute right-5 top-5 z-10")}>
+      <AntDesign name="closecircle" />
+    </TouchableOpacity>
+  );
+};
