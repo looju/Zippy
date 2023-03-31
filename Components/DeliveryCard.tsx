42b7f29f4c31d78a7a9e5dee9479e1026009bc9d
looju <omofade2020@gmail.com>
2023-03-31 13:03:59 +0100

    Card

--- Components/DeliveryCard.tsx
+++ Components/DeliveryCard.tsx
@@ -1,15 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { useTailwind } from "tailwind-rn/dist";
+import { Card } from "react-native-paper";
+import Entypo from "react-native-vector-icons/Entypo";
 
+type Params = {
+  order: Order;
+};
 
-type Params={
-    order:Order
-}
+export const DeliveryCard = ({ order }: Params) => {
+  const tw = useTailwind();
 
-export const DeliveryCard = ({order}:Params) => {
   return (
-    <View>
-      <Text>DeliveryCard</Text>
-    </View>
-  )
-}
+    <Card
+      style={[
+        tw("my-2 rounded-lg"),
+        {
+          padding: 0,
+          paddingTop: 16,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.2,
+          shadowRadius: 4,
+        },
+      ]}
+    >
+      <View>
+        <Entypo name="box" size={50} color={"#fff"} />
+      </View>
+    </Card>
+  );
+};
