37fe15ef80da49a4e7adf26c51da3d65dbbcfb1e
looju <omofade2020@gmail.com>
2023-03-31 13:25:33 +0100

    Deliverycard

--- Components/DeliveryCard.tsx
+++ Components/DeliveryCard.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { useTailwind } from "tailwind-rn/dist";
-import { Card } from "react-native-paper";
+import { Card, Divider } from "react-native-paper";
 import Entypo from "react-native-vector-icons/Entypo";
 
 type Params = {
@@ -22,11 +22,32 @@ export const DeliveryCard = ({ order }: Params) => {
           shadowOffset: { width: 0, height: 2 },
           shadowOpacity: 0.2,
           shadowRadius: 4,
+          backgroundColor: "#59c1cc",
         },
       ]}
     >
       <View>
         <Entypo name="box" size={50} color={"#fff"} />
+        <View>
+          <Text
+            style={tw("text-xs text-center uppercase text-white font-bold")}
+          >
+            {order.carrier}-{order.trackingId}
+          </Text>
+          <Text style={tw("text-lg text-center text-white font-bold")}>
+            Expected delivery date:
+            {new Date(order.createdAt).toLocaleDateString()} @
+            {new Date(order.createdAt).toTimeString()}
+          </Text>
+          <Divider style={{ backgroundColor: "#fff" }} />
+        </View>
+        <View>
+          <Text>Adress</Text>
+          <Text>
+            {order.Address}
+            {order.City}
+          </Text>
+        </View>
       </View>
     </Card>
   );
