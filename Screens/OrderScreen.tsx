bec7052b4ef63304c192f87ec6d24fc245c1aeba
looju <omofade2020@gmail.com>
2023-04-01 11:33:40 +0100

    OrderScreen

--- Screens/OrderScreen.tsx
+++ Screens/OrderScreen.tsx
@@ -1,30 +1,44 @@
-import { View, Text } from 'react-native'
-import React,{useState} from 'react'
-import { TabStackParamList } from '../Navigator/TabNavigator';
-import { RootStackParamList } from '../Navigator/RootNavigator';
-import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
-import { useTailwind } from 'tailwind-rn';
-import { useOrders } from './../Hooks/useOrders';
-
-
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { Button } from "react-native-paper";
+import { TabStackParamList } from "../Navigator/TabNavigator";
+import { RootStackParamList } from "../Navigator/RootNavigator";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { useTailwind } from "tailwind-rn";
+import { useOrders } from "./../Hooks/useOrders";
 
 type OrderScreenNavigationProp = CompositeNavigationProp<
-  BottomTabNavigationProp<TabStackParamList,"Orders">,
+  BottomTabNavigationProp<TabStackParamList, "Orders">,
   NativeStackNavigationProp<RootStackParamList>
 >;
 
-
 export const OrderScreens = () => {
-  const navigation=useNavigation<OrderScreenNavigationProp>()
-  const tw=useTailwind()
-  const { loading, error, orders }=useOrders()
-  const [ascending,setAscending]=useState<boolean>(false)
+  const navigation = useNavigation<OrderScreenNavigationProp>();
+  const tw = useTailwind();
+  const { loading, error, orders } = useOrders();
+  const [ascending, setAscending] = useState<boolean>(false);
 
   return (
-    <View>
-      <Text>OrderScreen</Text>
-    </View>
-  )
-}
+    <ScrollView style={{ backgroundColor: "#EB6A7C" }}>
+      <Image
+        source={{ uri: "https://links.papareact.com/m51" }}
+        style={tw("w-full h-64")}
+      />
+      <View>
+        <Button
+          onPress={() => setAscending(!ascending)}
+          buttonColor="pink"
+          textColor="#808080"
+          style={tw("py-2 px-4")}
+        >
+          {ascending ? "Showing:Older first" : "Showing: Most recent first"}
+        </Button>
+      </View>
+    </ScrollView>
+  );
+};
