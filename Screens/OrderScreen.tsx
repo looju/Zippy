50362ec2842b237531dd8b68c37e06616d9436c2
looju <omofade2020@gmail.com>
2023-04-01 12:09:43 +0100

    Sort

--- Screens/OrderScreen.tsx
+++ Screens/OrderScreen.tsx
@@ -38,6 +38,13 @@ export const OrderScreens = () => {
         >
           {ascending ? "Showing:Older first" : "Showing: Most recent first"}
         </Button>
+        {orders.sort((a, b) => {
+          if (ascending) {
+            return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1;
+          } else {
+            return new Date(a.createdAt) < new Date(b.createdAt) ? 1 : -1;
+          }
+        })}
       </View>
     </ScrollView>
   );
