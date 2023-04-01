0d4361ea91e9ec18beeec3b88130cbffe323b4dc
looju <omofade2020@gmail.com>
2023-04-01 13:01:00 +0100

    OrderCard

--- Components/OrderCard.tsx
+++ Components/OrderCard.tsx
@@ -2,6 +2,7 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { Card } from "@rneui/themed";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import Feather from "react-native-vector-icons/Feather";
 import { useTailwind } from "tailwind-rn";
 
 type Props = {
@@ -13,12 +14,18 @@ export const OrderCard = ({ key, item }: Props) => {
   const tw = useTailwind();
   return (
     <TouchableOpacity>
-      <Card containerStyle={tw('px-5 rounded-lg')}>
+      <Card containerStyle={tw("px-5 rounded-lg")}>
         <View style={tw("flex-row justify-between items-center")}>
           <View>
             <MaterialCommunityIcons name="truck-community" color={"#EB6A7C"} />
             <Text>{new Date(item.createdAt).toLocaleDateString()}</Text>
           </View>
+        </View>
+        <View style={tw("flex-row items-center")}>
+          <Text style={[tw("text-sm"), { color: "#EB6A7C" }]}>
+            {item.trackingItems.items.length}X
+          </Text>
+          <Feather name="box" style={tw("ml-2")} />
         </View>
       </Card>
     </TouchableOpacity>
