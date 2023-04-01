9df1ea5a588ea7ce5bb31b90ccc2b61b46b0c303
looju <omofade2020@gmail.com>
2023-04-01 11:12:37 +0100

    options

--- Navigator/TabNavigator.tsx
+++ Navigator/TabNavigator.tsx
@@ -1,9 +1,11 @@
 import React, { useLayoutEffect } from "react";
+import { Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { CustomerScreens } from "../Screens/CustomerScreens";
 import { OrderScreens } from "./../Screens/OrderScreen";
 import { useNavigation } from "@react-navigation/native";
 import Entypo from "react-native-vector-icons/Entypo";
+
 
 export type TabStackParamList = {
   Customers: undefined;
@@ -48,7 +50,12 @@ export const TabNavigator = () => {
         name="Customers"
         component={CustomerScreens}
       />
-      <Tab.Screen name="Orders" component={OrderScreens} />
+      <Tab.Screen name="Orders" component={OrderScreens} options={{
+        headerShown:false,
+        tabBarLabel:({focused,color})=>(
+          <Text style={{color:focused?"#EB6A7C":"gray"}}>Orders</Text>
+        )
+      }}/>
     </Tab.Navigator>
   );
 };
--- Screens/OrderScreen.tsx
+++ Screens/OrderScreen.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React,{useState} from 'react'
 import { TabStackParamList } from '../Navigator/TabNavigator';
 import { RootStackParamList } from '../Navigator/RootNavigator';
 import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { useTailwind } from 'tailwind-rn';
+import { useOrders } from './../Hooks/useOrders';
 
 
 
@@ -13,10 +15,13 @@ type OrderScreenNavigationProp = CompositeNavigationProp<
   NativeStackNavigationProp<RootStackParamList>
 >;
 
-const navigation=useNavigation<OrderScreenNavigationProp>()
-
 
 export const OrderScreens = () => {
+  const navigation=useNavigation<OrderScreenNavigationProp>()
+  const tw=useTailwind()
+  const { loading, error, orders }=useOrders()
+  const [ascending,setAscending]=useState<boolean>(false)
+
   return (
     <View>
       <Text>OrderScreen</Text>
